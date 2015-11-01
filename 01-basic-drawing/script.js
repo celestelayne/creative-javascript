2bf4ce32c3c3d188883cac4df7ee7af586853f2f
Celeste Layne <layne.celeste@gmail.com>
2015-11-01 15:32:03 -0800

    centered the canvas

--- 01-basic-drawing/script.js
+++ 01-basic-drawing/script.js
@@ -1,25 +1,25 @@
-var setup = function(){
-	// set up code goes here
-	var board = createCanvas(650, 480);
-	console.log(board);
-}
 
-var draw = function(){
-	// background
-	background(255, 204, 0);
+	var setup = function(){
+		// set up code goes here
+		var board = createCanvas(650, 480);
+		// console.log(board);
+	}
 
-	// ellipse
-	fill('rgb(64, 123, 255)');
-	noStroke();
-	ellipse(450, 190, 85, 85);
-	// rectangle
-	fill('rgb(226, 106, 106)');
-	noStroke();
-	rect(150, 150, 85, 85);
+	var draw = function(){
+		// background
+		background(255, 204, 0);
 
-}
+		// ellipse
+		fill('rgb(64, 123, 255)');
+		noStroke();
+		ellipse(450, 190, 85, 85);
+		// rectangle
+		fill('rgb(226, 106, 106)');
+		noStroke();
+		rect(150, 150, 85, 85);
 
-	var styleCanvas = function(){
+		// add CSS styles to drawing
 		var boardEl = document.getElementById("defaultCanvas0");
 		boardEl.classList.add('container');
+
 	}
